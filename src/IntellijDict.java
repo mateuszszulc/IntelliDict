c7295bf2f2a2469d72c582f42384504901a32078
Mateusz Szulc <mateusz.szulc@gmail.com>
2011-12-09 20:51:20 +0100

    First Initial Commit

---
+++ src/IntellijDict.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: mateusz
+ * Date: 06.12.11
+ * Time: 21:12
+ */
+
+public class IntellijDict {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/MainWindow.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: mateusz
+ * Date: 06.12.11
+ * Time: 21:26
+ */
+public class MainWindow {
+
+}

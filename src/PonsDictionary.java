35222832ad536b855bc43ad3f6d30b3dfdf7e6ab
U-vermont\mateusz <mateusz@vermont.(none)>
2012-03-05 22:23:05 +0100

    JSoup stubs.

--- src/PonsDictionary.java
+++ src/PonsDictionary.java
@@ -1,3 +1,11 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: mateusz
@@ -8,6 +16,47 @@ public class PonsDictionary {
     private final String ponsURL = null;
 
     public static String get(String entry) {
+
         return null;
     }
+
+    public void todo() {
+    String timetableURL = sourceUrlForTimetables + lineNumber + ".html";
+
+        Document doc = null;
+        try {
+            System.out.println("Debug");
+            doc = Jsoup.connect(timetableURL).get();
+            System.out.println("Debug");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Elements newsHeadlines = doc.select("#mp-itn b a");
+    }
+    public static List<BusStation> fetchPrimaryDirection(Document doc) {
+        Elements linia = doc.getElementsByClass("linia");
+        Element table = linia.first();
+        Elements tbody = table.getElementsByTag("tbody");
+        Elements trElements = tbody.first().getElementsByTag("tr");
+
+        trElements.remove(0);
+        trElements.remove(0);
+
+        for (Element busStationTr : trElements) {
+            Elements trChildrens = busStationTr.children();
+
+            BusStation busStation = new BusStation();
+            busStation.setRoute(trChildrens.get(0).text());
+            busStation.setSourceUrl(trChildrens.get(1).getElementsByTag("a").first().attr("href"));
+            busStation.setName(trChildrens.get(1).text());
+
+            System.out.println("*******************************");
+            System.out.print(trChildrens.get(1).getElementsByTag("a").first().attr("href") + "  ");
+            System.out.println(trChildrens.get(1).text() + "  ");
+        }
+
+        return null;
+    }
+
 }
